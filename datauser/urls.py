6dcd2d2163723850293565c3bfa3204687f869d2
Eder Lara <ederlatru@gmail.com>
2023-10-10 19:12:44 -0500

    Mejora en front

--- datauser/urls.py
+++ datauser/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import AcademyDetailView, AcamedyListView, AcademyCreateView, SkillsCreateView, HistoryCreateView, ProjectCreateView, AcademyCreateView
 
 urlpatterns =[
-    path('academy-list/', AcamedyListView.as_view(), name = 'academy-list'),
+    # path('academy-list/', AcamedyListView.as_view(), name = 'academy-list'),
     path('academy/<int:pk>/', AcademyDetailView.as_view(), name= 'academy-detail'),
     path('academy-add/', AcademyCreateView.as_view(), name= 'academy-create'),
     path('skills-add/', SkillsCreateView.as_view(), name= 'skills-create'),
