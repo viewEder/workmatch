43392350d687985bc26aab82e6f16b7b7bdceeb7
Eder Lara T <ederlatru@gmail.com>
2023-10-13 19:53:35 -0500

    Update Botones fix url

--- datauser/urls.py
+++ datauser/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from .views import AcademyDetailView, AcamedyListView, AcademyCreateView, SkillsCreateView, HistoryCreateView, ProjectCreateView, AcademyCreateView
 
 urlpatterns =[
-<<<<<<< HEAD
-=======
-
->>>>>>> f8dfa0bbee2fafdadb36d3c96ba9a94080613316
     path('academy/<int:pk>/', AcademyDetailView.as_view(), name= 'academy-detail'),
     path('academy-add/', AcademyCreateView.as_view(), name= 'academy-create'),
     path('skills-add/', SkillsCreateView.as_view(), name= 'skills-create'),
