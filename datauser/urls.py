e23d7cf0a1345db55d650cbba8f519551e695e34
Eder Lara T <ederlatru@gmail.com>
2023-10-13 19:56:59 -0500

    fix dataurls

--- datauser/urls.py
+++ datauser/urls.py
@@ -4,8 +4,4 @@ from .views import AcademyDetailView, AcamedyListView, AcademyCreateView
 urlpatterns =[
     path('academy/<int:pk>/', AcademyDetailView.as_view(), name= 'academy-detail'),
     path('academy-add/', AcademyCreateView.as_view(), name= 'academy-create'),
-<<<<<<< HEAD
 ]
-=======
-]
->>>>>>> 3e49c2810146763acfaf5a27b52d3e270546a905
